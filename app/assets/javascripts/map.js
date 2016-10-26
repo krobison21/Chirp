db9f3ab310bf444ece55ca2217e570dcc6a98708
mpeliche <mpeliche@tulane.edu>
2016-10-26 21:17:50 +0000

    implemented first stages of map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,10 +1,41 @@
-// JavaScript File
+$(document).on("ready", function() {
+  L.mapbox.accessToken = 'pk.eyJ1IjoibXBlbGljaGV0IiwiYSI6ImNpZ3doNWVvYzBzNXN2cW0zd2N5ZnBvY2YifQ.2RlTuIjDz1pLYOP4YpdKyw';
+  var map = L.mapbox.map('map', 'Your map layer', { zoomControl: false })
+  .setView([39.739, -104.990], 12);
 
-map = L.mapbox.map('map', 'your-map-id').setView([45.52086, -122.679523]
+  map.featureLayer.on("ready", function(e) {
+    getEvents(map);
+  });
+});
 
-$.ajax
-  dataType: 'text'
-  url: 'happy_hours/happening_now.json'
-  success: (data) ->
-    geojson = $.parseJSON(data)
-    map.featureLayer.setGeoJSON(geojson)
+function getEvents(map) {
+  var $loading_wheel = $("#spinning-wheel")
+  $loading_wheel.show();
+  $.ajax({
+    dataType: 'text',
+    url: '/events.json',
+    success:function(events) {
+      $loading_wheel.hide();
+      var geojson = $.parseJSON(events);
+      map.featureLayer.setGeoJSON({
+        type: "FeatureCollection",
+        features: geojson
+      });
+      addEventPopups(map);
+    },
+    error:function() {
+      $loading_wheel.hide();
+      alert("Could not load the events");
+    }
+  });
+}
+
+function addEventPopups(map) {
+  map.featureLayer.on("layeradd", function(e){
+    var marker = e.layer;
+    var properties = marker.feature.properties;
+    var popupContent = '<div class="marker-popup">' + '<h3>' + properties.title + '</h3>' +
+                       '<h4>' + properties.address + '</h4>' + '</div>';
+    marker.bindPopup(popupContent, {closeButton: false, minWidth: 300});
+  });
+}

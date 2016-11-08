c5cabe05256b9549a02766dcab986c415db4ec58
Katy Robison <katyrobison21@gmail.com>
2016-11-08 19:59:31 +0000

    Merged rspec branch into master

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ OmniAuth.config.logger = Rails.logger
  
 Rails.application.config.middleware.use OmniAuth::Builder do
 
-  provider :facebook, '1772685836331864', 'ec4c79deae51eb0a555ca30ceefc4353', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
+  provider :facebook, 'APP ID', 'SECRET KEY', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
 
 end

e5c5503bda9890d0ac922e6632cc13fd1a083913
Perri <plevine1@tulane.edu>
2016-11-09 22:09:30 +0000

    Adding some css, not ready for push to master

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
  
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1772685836331864', 'ba37853a87dcd79c202090e5d28cb6ca', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
+  provider :facebook, '331863843848775', '5b4e11b495140d25374d99c2666f3a80', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
 end

22fd95a5167663cfc5db18cdca9f3c4c7dae7510
Katy Robison <katyrobison21@gmail.com>
2016-11-16 23:44:41 +0000

    Resolving Merge conflict from map edits

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,14 +78,10 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.serve_static_assets = true
-<<<<<<< HEAD
-  RAILS_ENV=production bundle exec rake assets:precompile
-=======
   
   #RUN IN COMMAND WINDOW:
   #RAILS_ENV=production bundle exec rake assets:precompile
   #heroku run rake assets:precompile
 
->>>>>>> master
 
 end
